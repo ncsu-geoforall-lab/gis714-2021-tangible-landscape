a5a7d4db7f16f2fe5643f240478e2df8590b6fe5
John Polo <68556651+John-Polo@users.noreply.github.com>
2021-02-23 21:12:38 -0500

    Update activity_johnpolo.py
    Get rid of whitespace

--- activities/activity_johnpolo.py
+++ activities/activity_johnpolo.py
@@ -17,7 +17,7 @@ import grass.script as gs
 
 def run_topo_par(scanned_elev, morphmap, select_meth, env, **kwargs):
     gs.run_command('r.param.scale', input=scanned_elev, output=morphmap, method=select_meth, env=env, overwrite=True)
-    
+
 
 # this part is for testing without TL
 def main():
@@ -30,8 +30,9 @@ def main():
 
     elevation = "elev_lid792_1m"
     select_meth = 'profc'
-    
+
     run_topo_par(scanned_elev=elevation, morphmap='feat_verticalcurv', env=env, select_meth=select_meth)
 
 if __name__ == "__main__":
     main()
+
