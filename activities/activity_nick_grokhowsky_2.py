bb970bff8ffec8b5ed5f7296882be54dab1d7102
nickgrok <nickgrok@gmail.com>
2021-04-10 18:20:29 -0400

    last correction

--- activities/activity_nick_grokhowsky_2.py
+++ activities/activity_nick_grokhowsky_2.py
@@ -23,13 +23,13 @@ import grass.script as gs
 # Function to show significantly high and low pixels
 def run_significantValues(elev, env, **kwargs):
     # Create rules for r.recode
-    rules = ["*-2.58: -3:-3",
-            "-2.57999:-1.96:-2:-2",
-            "-1.95999:-1.65:-1:-1",
-            "-1.64999:1.65:0:0",
-            "1.651:1.96:1:1",
-            "1.961:2.58:2:2",
-            "2.581*:3:3"]
+    rules = ["*:-2.58: -3:-3",
+            "-2.58:-1.96:-2:-2",
+            "-1.96:-1.65:-1:-1",
+            "-1.65:1.65:0:0",
+            "1.65:1.96:1:1",
+            "1.96:2.58:2:2",
+            "2.58:*:3:3"]
 
     gs.run_command(
         "r.neighbors", input=elev, output="smooth", method="average", flags="c", env=env
