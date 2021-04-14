3bbc1098237ebdf46e4cf0f380de89477d3f9a60
arielsaffer <61440835+arielsaffer@users.noreply.github.com>
2021-04-14 14:24:12 -0400

    a bit cleaner :)

--- activities/activity_arielsaffer.py
+++ activities/activity_arielsaffer.py
@@ -15,72 +15,46 @@ def run_radiance_now(scanned_elev, env, **kwargs):
 
         obj = TimezoneFinder()
 
-        def run_radiance_now(scanned_elev, env, **kwargs):
-            # Get the current day/time
-            today = datetime.datetime.today()
-            # Get the lat and lon of the NE corner of the computation region
-            map_loc = gs.read_command("g.region", flags="bg", raster=scanned_elev)
-            long = float(map_loc.split()[4:6][0].split("=")[1])
-            lat = float(map_loc.split()[4:6][1].split("=")[1])
-            # Identify the time zone of the mapped region
-            map_timezone = obj.timezone_at(lng=long, lat=lat)
-            # Convert the current day/time to the time in that timezone
-            maptime = today.astimezone(pytz.timezone(map_timezone))
-            # Extract day of year and hour
-            day = maptime.timetuple().tm_yday
-            current_time = maptime.hour
-
-            # precompute slope and aspect
-            gs.run_command(
-                "r.slope.aspect",
-                elevation=scanned_elev,
-                slope="slope",
-                aspect="aspect",
-                env=env,
-            )
-            gs.run_command(
-                "r.sun",
-                elevation=scanned_elev,
-                slope="slope",
-                aspect="aspect",
-                beam_rad="beam",
-                day=day,
-                time=current_time,
-                env=env,
-            )
-
-            # extract shade and set color to black and white
-            gs.mapcalc("shade = if(beam == 0, 0, 1)", env=env)
-            gs.run_command("r.colors", map="beam", color="grey")
-
+        # Get the current day/time
+        today = datetime.datetime.today()
+        # Get the lat and lon of the NE corner of the computation region
+        map_loc = gs.read_command("g.region", flags="bg", raster=scanned_elev)
+        long = float(map_loc.split()[4:6][0].split("=")[1])
+        lat = float(map_loc.split()[4:6][1].split("=")[1])
+        # Identify the time zone of the mapped region
+        map_timezone = obj.timezone_at(lng=long, lat=lat)
+        # Convert the current day/time to the time in that timezone
+        maptime = today.astimezone(pytz.timezone(map_timezone))
+        # Extract day of year and hour
+        day = maptime.timetuple().tm_yday
+        current_time = maptime.hour
     except:
         # Get the current day/time
         today = datetime.datetime.today()
         day = today.timetuple().tm_yday
         current_time = today.hour
-
         # precompute slope and aspect
-        gs.run_command(
-            "r.slope.aspect",
-            elevation=scanned_elev,
-            slope="slope",
-            aspect="aspect",
-            env=env,
-        )
-        gs.run_command(
-            "r.sun",
-            elevation=scanned_elev,
-            slope="slope",
-            aspect="aspect",
-            beam_rad="beam",
-            day=day,
-            time=current_time,
-            env=env,
-        )
+    gs.run_command(
+        "r.slope.aspect",
+        elevation=scanned_elev,
+        slope="slope",
+        aspect="aspect",
+        env=env,
+    )
+    gs.run_command(
+        "r.sun",
+        elevation=scanned_elev,
+        slope="slope",
+        aspect="aspect",
+        beam_rad="beam",
+        day=day,
+        time=current_time,
+        env=env,
+    )
 
-        # extract shade and set color to black and white
-        gs.mapcalc("shade = if(beam == 0, 0, 1)", env=env)
-        gs.run_command("r.colors", map="beam", color="grey")
+    # extract shade and set color to black and white
+    gs.mapcalc("shade = if(beam == 0, 0, 1)", env=env)
+    gs.run_command("r.colors", map="beam", color="grey")
 
 
 # this part is for testing without TL
