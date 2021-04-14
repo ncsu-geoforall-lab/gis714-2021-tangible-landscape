6d935be76dd5b0f4734588998c02ef666c63f653
arielsaffer <61440835+arielsaffer@users.noreply.github.com>
2021-04-14 14:11:07 -0400

    now with a package-free option

--- activities/activity_arielsaffer.py
+++ activities/activity_arielsaffer.py
@@ -1,44 +1,95 @@
 import grass.script as gs
 import datetime
-# you may need to install this package
-import pytz 
-# you probably need to install this package
-from timezonefinder import TimezoneFinder 
-obj = TimezoneFinder()
 
-# compute solar irradiance (W/m2) for a given day and hour 
-# and extract the shades cast by topography, using the time zone
-# of the surface location (based on NE corner)
+try:
+    # you may need to install this package
+    import pytz
 
-def run_radiance_now(scanned_elev, env, **kwargs):
+    # you probably need to install this package
+    from timezonefinder import TimezoneFinder
 
-    # Get the current day/time
-    today = datetime.datetime.today()
-    # Get the lat and lon of the NE corner of the computation region
-    map_loc = gs.read_command('g.region',flags='bg',raster=scanned_elev)
-    long = float(map_loc.split()[4:6][0].split('=')[1])
-    lat = float(map_loc.split()[4:6][1].split('=')[1])
-    # Identify the time zone of the mapped region
-    map_timezone = obj.timezone_at(lng=long, lat=lat)
-    # Convert the current day/time to the time in that timezone
-    maptime = today.astimezone(pytz.timezone(map_timezone))
-    # Extract day of year and hour
-    day = maptime.timetuple().tm_yday
-    current_time = maptime.hour
+    obj = TimezoneFinder()
+    # compute solar irradiance (W/m2) for a given day and hour
+    # and extract the shades cast by topography, using the time zone
+    # of the surface location (based on NE corner)
+    def run_radiance_now(scanned_elev, env, **kwargs):
+        # Get the current day/time
+        today = datetime.datetime.today()
+        # Get the lat and lon of the NE corner of the computation region
+        map_loc = gs.read_command("g.region", flags="bg", raster=scanned_elev)
+        long = float(map_loc.split()[4:6][0].split("=")[1])
+        lat = float(map_loc.split()[4:6][1].split("=")[1])
+        # Identify the time zone of the mapped region
+        map_timezone = obj.timezone_at(lng=long, lat=lat)
+        # Convert the current day/time to the time in that timezone
+        maptime = today.astimezone(pytz.timezone(map_timezone))
+        # Extract day of year and hour
+        day = maptime.timetuple().tm_yday
+        current_time = maptime.hour
 
-    # precompute slope and aspect
-    gs.run_command('r.slope.aspect', elevation=scanned_elev, slope='slope', aspect='aspect', env=env)
-    gs.run_command('r.sun', elevation=scanned_elev, slope='slope', aspect='aspect', beam_rad='beam', day=day, time=current_time, env=env)
+        # precompute slope and aspect
+        gs.run_command(
+            "r.slope.aspect",
+            elevation=scanned_elev,
+            slope="slope",
+            aspect="aspect",
+            env=env,
+        )
+        gs.run_command(
+            "r.sun",
+            elevation=scanned_elev,
+            slope="slope",
+            aspect="aspect",
+            beam_rad="beam",
+            day=day,
+            time=current_time,
+            env=env,
+        )
 
-    # extract shade and set color to black and white
-    gs.mapcalc("shade = if(beam == 0, 0, 1)", env=env)
-    gs.run_command('r.colors', map='beam', color='grey')
+        # extract shade and set color to black and white
+        gs.mapcalc("shade = if(beam == 0, 0, 1)", env=env)
+        gs.run_command("r.colors", map="beam", color="grey")
+
+
+except:
+    # if we can't use those packages, assume the current timezone
+    def run_radiance_now(scanned_elev, env, **kwargs):
+
+        # Get the current day/time
+        today = datetime.datetime.today()
+        day = today.timetuple().tm_yday
+        current_time = today.hour
+
+        # precompute slope and aspect
+        gs.run_command(
+            "r.slope.aspect",
+            elevation=scanned_elev,
+            slope="slope",
+            aspect="aspect",
+            env=env,
+        )
+        gs.run_command(
+            "r.sun",
+            elevation=scanned_elev,
+            slope="slope",
+            aspect="aspect",
+            beam_rad="beam",
+            day=day,
+            time=current_time,
+            env=env,
+        )
+
+        # extract shade and set color to black and white
+        gs.mapcalc("shade = if(beam == 0, 0, 1)", env=env)
+        gs.run_command("r.colors", map="beam", color="grey")
 
 
 # this part is for testing without TL
 
+
 def main():
     import os
+
     # get the current environment variables as a copy
     env = os.environ.copy()
     # we want to run this repetetively without deleted the created files
@@ -50,7 +101,9 @@ def main():
     # resampling to have similar resolution as with TL
 
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
-    run_radiance_now(scanned_elev=elevation, env=env)
+    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+    run_radiance_now(scanned_elev=elev_resampled, env=env)
+
 
 if __name__ == "__main__":
     main()
