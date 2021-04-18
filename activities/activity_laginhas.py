02d3b7f30662d4f66d2c225d818691fc0f236866
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2021-04-18 18:01:38 -0400

    Update activity_laginhas.py

--- activities/activity_laginhas.py
+++ activities/activity_laginhas.py
@@ -5,11 +5,25 @@ This function visualizes differences in a file that has been resampled to a coar
 """
 import grass.script as gs
 
+
 def run_resamplediffs(scanned_elev, env, **kwargs):
-    gs.run_command('r.resamp.interp', input='elevation', output='elev_4nn', method='nearest', overwrite=True)
-    gs.run_command('r.resamp.interp', input='elevation', output='elev_4bi', method='bilinear', overwrite=True)
-    gs.mapcalc('elev_diff=elev_4nn - elev_4bi', env=env, overwrite=True)
-    gs.run_command('r.colors.stddev', map='elev_diff', env=env)
+    gs.run_command(
+        "r.resamp.interp",
+        input="elevation",
+        output="elev_4nn",
+        method="nearest",
+        overwrite=True,
+    )
+    gs.run_command(
+        "r.resamp.interp",
+        input="elevation",
+        output="elev_4bi",
+        method="bilinear",
+        overwrite=True,
+    )
+    gs.mapcalc("elev_diff=elev_4nn - elev_4bi", env=env, overwrite=True)
+    gs.run_command("r.colors.stddev", map="elev_diff", env=env)
+
 
 # this part is for testing without TL
 def main():
@@ -25,6 +39,7 @@ def main():
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
     gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
     run_resamplediffs(scanned_elev=elevation, env=env)
+
 
 if __name__ == "__main__":
     main()
